7c2114304b122c96f3e2919168495dd236af50f5
saifDeveloper <saikhsaifali@gmail.com>
2021-03-19 10:41:06 +0530

    questionEntity added

---
+++ quora-service/src/main/java/com/upgrad/quora/service/entity/QuestionEntity.java
@@ -0,0 +1,102 @@
+package com.upgrad.quora.service.entity;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * This class maps to question table in database
+ * @author saif
+ */
+@Entity
+@Table(name = "question")
+@NamedQueries(
+        {
+                @NamedQuery(name = "questionByUuid", query = "select question from QuestionEntity question where question.uuid = :uuid"),
+                @NamedQuery(name = "getAllQuestion", query = "select qe from QuestionEntity qe"),
+                @NamedQuery(name = "getAllQuestionByUser", query = "select qe from QuestionEntity qe where qe.user.uuid = :uuid")
+        }
+)
+public class QuestionEntity implements Serializable {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "uuid")
+    @Size(max = 200)
+    private String uuid;
+
+    @Column(name = "content")
+    @Size(max = 200)
+    private String content;
+
+    // @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "date", length = 6)
+    private LocalDateTime date;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return new EqualsBuilder().append(this, obj).isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(this).hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);}
+}
